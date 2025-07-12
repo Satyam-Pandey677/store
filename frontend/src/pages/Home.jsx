752185d661f5eb4cc0b96f3461ba59402330528a
Satyam-pandey677 <satyampandey0241@gmail.com>
2025-07-12 12:31:33 +0530

    Refactor Home and ProductCrousel components: update state variables for product queries, enhance loading and error handling, and improve product display structure.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,12 +6,25 @@ import Message from '../component/Message';
 
 const Home = () => {
   const {keyword} = useParams()
-  const {data, isLoading, error} = useGetProductQuery({keyword})
+  const {data, isLoading, isError} = useGetProductQuery({keyword})
   
   
   return (
     <>
       {!keyword ? <Header/> : null }
+      {isLoading ? (<isLoading/>): isError ? (<Message variant='danger'>{isError.error}</Message>):(
+        <>
+          <div className="flex justify-between items-center">
+            <h1 className='ml-[20rem] mt-[10rem] text-[3rem]'>
+              Speacial Products
+            </h1>
+
+            <Link to="/shop" className='bg-pink-600 font-bold rounded-full py-2 px-10 mr-[18rem] mt-[10rem]'>
+                Shop
+            </Link>
+          </div>
+        </>
+      )  }
     </>
   )
 }
--- frontend/src/pages/products/ProductCrousel.jsx
+++ frontend/src/pages/products/ProductCrousel.jsx
@@ -13,8 +13,8 @@ import {
 } from "react-icons/fa"
 
 const ProductCrousel = () => {
-    const {products, isLoading, error} = useGetTopProductQuery()
-    console
+    const {data, isLoading, error} = useGetTopProductQuery()
+    console.log(data)
 
     const settings = {
         dots:false,
@@ -34,9 +34,44 @@ const ProductCrousel = () => {
                 {error?.data?.message || error.message}
             </Message>
         ) : <Slider {...settings} className="xl:w-[50rem] lg:w-[50rem] md:w-[56rem] sm:w-[40rem] sm:block">
-                {/* {products?.data.map((product) => (
+                {data?.data.map(({image, _id, name, price,discription, brand, createdAt, numReviews, rating, quantity, countInStock}) => (
+                    <div key={_id} >
+                        <img src={image} alt={name} className="w-full rounded-lg object-cover h-[30rem]" />
+                        <div className="flex justify-between w-[20rem]">
+                            <div className="one">
+                                <h2>{name}</h2>
+                                <p>${price}</p> <br />
+                                <p className="w-[25rem]">{discription.substring(0,170)}...</p>
+                            </div>
+                            <div className="flex justify-between w-[20rem]">
+                                <div className="one">
+                                    <h1 className="flex items-center mb-6 w-[8rem]">
+                                        <FaStore className="mr-2"/> Brand: {brand}
+                                    </h1>
+                                    <h1 className="flex items-center mb-6 w-[10rem]">
+                                        <FaClock className="mr-2"/> Added:{" "} {moment(createdAt).fromNow()}
+                                    </h1>
+                                    <h1 className="flex items-center mb-6 w-[8rem]">
+                                        <FaStar className="mr-2"/> Reviews: {numReviews}
+                                    </h1>
+                                </div>
 
-                ))} */}
+                                <div className="two">
+                                    <h1 className="flex item-center mb-6 w-[7rem]">
+                                        <FaStar className="mr-2 mt-1 "/> Ratings:{" "} {Math.round(rating)}
+                                    </h1>
+                                    <h1 className="flex item-center mb-6 w-[7rem]">
+                                        <FaShoppingCart className="mr-2 mt-1 "/> Quantity:{" "} {quantity}
+                                    </h1>
+                                    <h1 className="flex item-center mb-6 w-[7rem]">
+                                        <FaBox className="mr-2 mt-1 "/> In Stock:{" "} {countInStock}
+                                    </h1>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                ))}
             </Slider>}
     </div>
   )
