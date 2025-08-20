3c3e1f4c3c98b421c19f95771636909d0d66b5f2
sanspandey <satyampandey0241@gmail.com>
2025-08-20 23:30:14 +0530

    Add addToCart functionality and product description display in ProductCard component; enhance UI with read more link and cart button.

--- frontend/src/pages/products/ProductCard.jsx
+++ frontend/src/pages/products/ProductCard.jsx
@@ -8,6 +8,13 @@ import HeartIcon from "./HeartIcon"
 const ProductCard = ({p}) => {
     console.log(p)
     const dispatch = useDispatch()
+
+    const addToCartHandler = (product, qty) => {
+        dispatch(addToCart({...product, qty}))
+        toast.success("Item added successfully", {
+            autoClose:2000
+        })
+    }   
   return (
     <div className="max-w-sm relative bg-[#1A1A1A] rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <section className="relative">
@@ -36,6 +43,36 @@ const ProductCard = ({p}) => {
                     })}
                 </p>
             </div>
+            <p className="mb-3 font-normal text-[#CFCFCF]">
+                {p?.discription?.substring(0,60)} ...
+            </p>
+            <section className="flex justify-between items-center">
+                <Link 
+                    to={`/product/${p._id}`}
+                    className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-pink-700 rounded-lg hover:bg-pink-800 focus:ring-4 focus:outline-none focus:ring-pink-300 dark:bg-pink-600 dark:hover:bg-pink-700 dark:focus:ring-pink-800"  
+                >
+                    read more
+                     <svg
+              className="w-3.5 h-3.5 ml-2"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 14 10"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M1 5h12m0 0L9 1m4 4L9 9"
+              />
+            </svg>
+                </Link>
+
+                <button className="p-2 rounded-full cursor-pointer" onClick={() => addToCartHandler(p, 1)}>
+                    <AiOutlineShoppingCart className="text-white" size={25}/>
+                </button>
+            </section>
         </div>
     </div>
   )
