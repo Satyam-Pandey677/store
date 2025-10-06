bd64954a75cbc972c9cf6bdffeda01a0bfca7116
Satyam-Pandey677 <164221608+Satyam-Pandey677@users.noreply.github.com>
2025-10-06 19:46:35 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   server: {
   proxy: {
     '/api': {
-      target: 'https://store-1-33c5.onrender.com',
+      target: 'https://store-xe8y.onrender.com',
       changeOrigin: true,
       secure: false,
     },
