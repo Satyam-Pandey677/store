49995f5201119f30d6266b95eae6b5f4b017dfc5
Satyam-Pandey677 <satyampandey0241@gmail.com>
2025-10-15 15:05:00 +0530

    Update static file serving and routing in index.js; configure build output directory in vite.config.js

--- backend/index.js
+++ backend/index.js
@@ -32,8 +32,12 @@ app.get("/api/config/paypal", (req, res) => {
     res.send({clientId: process.env.PAYPAL_CLIENT_ID});
 })
 
-const __dirname = path.resolve()
-// app.use('/uploads', express.static(path.join(__dirname + '/uploads')))
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
 
 app.listen(port, () => {
     console.log("Server running at Port : ",port)
