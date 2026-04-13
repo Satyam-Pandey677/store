1c195ba88c5cb8c72fbd1b202743cf7bdc8278fc
Satyam-Pandey677 <satyampandey0241@gmail.com>
2026-04-13 12:28:49 +0530

    fix: add cursor pointer to checkout button in Cart component

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -82,7 +82,7 @@ const Cart = () => {
                   </div>
 
                   <button 
-                  className="bg-pink-400 mt-4 py-2 px-4 rounded-full text-lg w-full"
+                  className="bg-pink-400 mt-4 py-2 px-4 rounded-full text-lg w-full cursor-pointer"
                   disabled={cartItems.length == 0} 
                   onClick={checkOutHandler}
                   >
